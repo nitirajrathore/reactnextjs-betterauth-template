e0f14e03d56ab5caa675315043f58d6a73b20f72
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-01-03 16:13:58 +0530

    Initial commit from other project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Dashboard() {
+    return (
+        <div className="w-full">
+            <div className="flex gap-4 flex-col">
+                Welcome to the dashboard!
+            </div>
+        </div>
+    )
+}
