c75edfc24180479c05194face31f4f3192284e47
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-01-04 19:21:01 +0530

    Added admin button

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,8 +4,10 @@ import { redirect } from "next/navigation";
 import UserCard from "./user-card";
 import { OrganizationCard } from "./organization-card";
 import AccountSwitcher from "@/components/account-switch";
+import { Link } from "lucide-react";
+import {AdminBtn} from "@/components/admin-btn";
 
-export default async function DashboardPage() {
+export default async function ProfilePage() {
 	const [session, activeSessions] =
 		await Promise.all([
 			auth.api.getSession({
@@ -14,29 +16,19 @@ export default async function DashboardPage() {
 			auth.api.listSessions({
 				headers: await headers(),
 			}),
-			// auth.api.listDeviceSessions({
-			// 	headers: await headers(),
-			// }),
-			// auth.api.getFullOrganization({
-			// 	headers: await headers(),
-			// }),
 		]).catch((e) => {
 			throw redirect("/sign-in");
 		});
 	return (
 		<div className="w-full">
 			<div className="flex gap-4 flex-col">
-				{/* <AccountSwitcher
-					sessions={JSON.parse(JSON.stringify(deviceSessions))}
-				/> */}
 				<UserCard
 					session={JSON.parse(JSON.stringify(session))}
 					activeSessions={JSON.parse(JSON.stringify(activeSessions))}
 				/>
-				{/* <OrganizationCard
-					session={JSON.parse(JSON.stringify(session))}
-					activeOrganization={JSON.parse(JSON.stringify(organization))}
-				/> */}
+				{session?.user.role === "admin" && (
+					<AdminBtn />
+				)}
 			</div>
 		</div>
 	);
